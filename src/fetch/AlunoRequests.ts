4ab8fe527fcccb08db994092ccb061c9b70fba87
Enzo Cassão Marini <enzocassao@gmail.com>
2026-04-08 16:28:34 -0300

    feat: criado o arquivo "AlunoRequests.ts" na pasta "fetch"

--- src/fetch/AlunoRequests.ts
+++ src/fetch/AlunoRequests.ts
@@ -1,1 +1,33 @@
 // Classe responsável por fazer requisições à API - aluno
+class AlunoRequests {
+    private serverUrl: string;
+    private endpointListarAluno: string;
+
+    constructor() {
+        this.serverUrl = 'http://localhost:3333';
+        this.endpointListarAluno = '/api/aluno';
+    }
+
+    async listarAluno() {
+        const token = localStorage.getItem('token');
+        try {
+            const response = await fetch(`${this.serverUrl}${this.endpointListarAluno}`, {
+                headers: {
+                    'x-access-token': `${token}`
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Não foi possível listar o aluno.');
+            }
+
+            return response.json();
+        } catch (error) {
+            console.error(`Erro ao fazer consulta à API: ${error}`);
+            return null;
+        }
+    }
+
+}
+
+export default new AlunoRequests();
