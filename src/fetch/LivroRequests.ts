e69e51bb65ab5b063dcca8cd1e4985a2769c1f70
Enzo Cassão Marini <enzocassao@gmail.com>
2026-04-08 16:30:58 -0300

    feat: criado o arquivo "LivroRequests.ts" na pasta "fetch"

--- src/fetch/LivroRequests.ts
+++ src/fetch/LivroRequests.ts
@@ -1,1 +1,33 @@
 // Classe responsável por fazer requisições à API - livro
+class LivroRequests {
+    private serverUrl: string;
+    private endpointListarLivros: string;
+
+    constructor() {
+        this.serverUrl = 'http://localhost:3333';
+        this.endpointListarLivros = '/api/livros';
+    }
+
+    async listarLivros() {
+        const token = localStorage.getItem('token');
+        try {
+            const response = await fetch(`${this.serverUrl}${this.endpointListarLivros}`, {
+                headers: {
+                    'x-access-token': `${token}`
+                }
+            });
+
+            if(!response.ok) {
+                throw new Error('Não foi possível listar as livros.');
+            }
+
+            return response.json();
+        } catch (error) {
+            console.error(`Erro ao fazer consulta à API: ${error}`);
+            return null;
+        }
+    }
+
+}
+
+export default new LivroRequests();
