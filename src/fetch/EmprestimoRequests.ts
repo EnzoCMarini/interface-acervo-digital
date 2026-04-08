fe97309250d199aa1095d0835b8a8c72f53a0cef
Enzo Cassão Marini <enzocassao@gmail.com>
2026-04-08 16:30:36 -0300

    feat: criado o arquivo "EmprestimoRequests.ts" na pasta "fetch"

--- src/fetch/EmprestimoRequests.ts
+++ src/fetch/EmprestimoRequests.ts
@@ -1,1 +1,33 @@
 // Classe responsável por fazer requisições à API - emprestimo
+class EmprestimoRequests {
+    private serverUrl: string;
+    private endpointListarEmprestimos: string;
+
+    constructor() {
+        this.serverUrl = 'http://localhost:3333';
+        this.endpointListarEmprestimos = '/api/emprestimos';
+    }
+
+    async listarEmprestimos() {
+        const token = localStorage.getItem('token');
+        try {
+            const response = await fetch(`${this.serverUrl}${this.endpointListarEmprestimos}`, {
+                headers: {
+                    'x-access-token': `${token}`
+                }
+            });
+
+            if(!response.ok) {
+                throw new Error('Não foi possível listar as Emprestimos.');
+            }
+
+            return response.json();
+        } catch (error) {
+            console.error(`Erro ao fazer consulta à API: ${error}`);
+            return null;
+        }
+    }
+
+}
+
+export default new EmprestimoRequests();
