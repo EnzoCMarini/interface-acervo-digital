7a14692b32245b49a83481fd7df7a5abd1c73d4b
Enzo Cassão Marini <enzocassao@gmail.com>
2026-04-15 09:03:29 -0300

    feat: adicionado as rotas das paginas de login, listagem de alunos, listagem de livros e listagem de emprestimos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PHome from './pages/PHome/PHome'
+import PLogin from './pages/PLogin/PLogin'
+import PListagemAluno from './pages/PListagem/PListagemAluno/PListagemAluno'
+import PListagemEmprestimo from './pages/PListagem/PListagemEmprestimo/PListagemEmprestimo'
+import PListagemLivro from './pages/PListagem/PListagemLivro/PListagemLivro'
 
 function App() {
 
@@ -8,6 +12,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<PHome />} /> 
+        <Route path='/login' element={<PLogin />} />
+        <Route path='/listagemalunos' element={<PListagemAluno />} />
+        <Route path='/listagemlivros' element={<PListagemLivro />} />
+        <Route path='/listagememprestimos' element={<PListagemEmprestimo />} />
       </Routes>
     </BrowserRouter>
   )
