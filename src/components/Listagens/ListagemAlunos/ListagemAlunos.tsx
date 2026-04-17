5f0bae19d8a1bbb8a463eb667752d43b7b1ea056
Enzo Cassão Marini <enzocassao@gmail.com>
2026-04-17 16:34:36 -0300

    feat: feito a conexão com o banco de dados com a Listagem de Alunos

--- src/components/Listagens/ListagemAlunos/ListagemAlunos.tsx
+++ src/components/Listagens/ListagemAlunos/ListagemAlunos.tsx
@@ -1,48 +1,64 @@
 import { type JSX } from "react";
+import { useState, useEffect } from "react";
+import AlunoRequests from "../../../fetch/AlunoRequests";
+import type AlunoDTO from "../../../dto/AlunoDTO";
 
 function ListagemAlunos(): JSX.Element {
-    return (
-        <main> {/* Web Semântica SEO (Search Engine Optimizer) */}
-            <h1>Alunos</h1>
+    const [alunos, setAlunos] = useState<AlunoDTO[]>([]);
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>RA</th>
-                        <th>Nome</th>
-                        <th>E-mail</th>
-                        <th>Telefone</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>A123456</td>
-                        <td>Felisberto Felis</td>
-                        <td>felisberto@email.com</td>
-                        <td>(67) 6 7676-7676</td>
-                        <td>
-                            <a href="#">Atualizar</a>
-                            <a href="#">Detalhes</a>
-                            <a href="#">Deletar</a>
-                        </td>
-                    </tr>
-                    <tr>
-                    <td>2</td>
-                        <td>A654321</td>
-                        <td>Trizteberto Trizte</td>
-                        <td>trizteberto@email.com</td>
-                        <td>(69) 6 9696-9696</td>
-                        <td>
-                            <a href="#">Atualizar</a>
-                            <a href="#">Detalhes</a>
-                            <a href="#">Deletar</a>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+    useEffect(() => {
+        const buscarAlunos = async () => {
+            try {
+                const listaDeAlunos = await AlunoRequests.listarAlunos();
+                setAlunos(listaDeAlunos);
+            } catch (error) {
+                console.error(`Erro ao buscar alunos. ${error}`);
+                alert("Erro ao criar a listagem de alunos.");
+            }
+        }
+
+        buscarAlunos();
+    }, []);
+
+    return (
+        <main className="bg-gray-200 h-[76vh]"> {/* Web Semântica SEO (Search Engine Optimizer) */}
+            <div className="w-8/10 flex m-auto p-12">
+                <h1 className="w-9/10 text-3xl text-center">Alunos</h1>
+                <a href="#" className="w-1/10 p-3 text-md bg-slate-700 rounded-md text-center text-white font-bold flex items-center justify-center hover:cursor-pointer">
+                    Novo Aluno
+                </a>
+            </div>
+
+            <div className="w-8/10 max-w-[80%] max-h-7/10 overflow-auto overscroll-none m-auto border border-slate-800">
+                <table className="table-auto w-full border-collapse text-sm">
+                    <thead className="bg-slate-700 sticky top-0 z-10">
+                        <tr>
+                            <th className="border border-slate-600 text-white">ID</th>
+                            <th className="border border-slate-600 text-white p-4">RA</th>
+                            <th className="border border-slate-600 text-white">Nome</th>
+                            <th className="border border-slate-600 text-white">E-mail</th>
+                            <th className="border border-slate-600 text-white">Telefone</th>
+                            <th className="border border-slate-600 text-white">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody> {/* Dados fictícios (por enquanto) */}
+                        {alunos.map((aluno) => (
+                            <tr className="border-b-2 text-center odd:bg-slate-300 even:bg-slate-100 hover:bg-slate-600 hover:text-white hover:cursor-pointer" key={aluno.id_aluno}>
+                                <td>{aluno.id_aluno}</td>
+                                <td className="p-3">{aluno.ra}</td>
+                                <td>{aluno.nome} {aluno.sobrenome}</td>
+                                <td>{aluno.email}</td>
+                                <td>{aluno.celular}</td>
+                                <td>
+                                    <a href="#" className="inline-block bg-sky-600 p-2 m-2 w-1/5 rounded-md text-white text-center">Detalhes</a>
+                                    <a href="#" className="inline-block bg-emerald-400 p-2 m-2 w-1/5 rounded-md text-white">Atualizar</a>
+                                    <a href="#" className="inline-block bg-red-600 p-2 m-2 w-1/5 rounded-md text-white">Deletar</a>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </main>
     );
 }
