47789a81efb09c25d3bb787d94506479b27d0bb7
Enzo Cassão Marini <enzocassao@gmail.com>
2026-05-08 15:22:57 -0300

    feat: adicionado a pagina de detalhes nos emprestimos

--- src/fetch/EmprestimoRequests.ts
+++ src/fetch/EmprestimoRequests.ts
@@ -1,4 +1,6 @@
 // Classe responsável por fazer requisições à API - emprestimo
+import type EmprestimoDTO from "../dto/EmprestimoDTO";
+
 class EmprestimoRequests {
     private serverURL;
     private endpointEmprestimo;
@@ -27,6 +29,28 @@ class EmprestimoRequests {
             }
         } catch (error) {
             console.error(`Erro ao fazer a consulta de empréstimos. ${error}`);
+            return;
+        }
+    }
+
+    async obterEmprestimoPorId(id_emprestimo: number): Promise<EmprestimoDTO | undefined> {
+        try {
+            const token = localStorage.getItem('token');
+            const respostaAPI = await fetch(`${this.serverURL}${this.endpointEmprestimo}/${id_emprestimo}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': `${token}`
+                }
+            });
+
+            if (respostaAPI.ok) {
+                const Emprestimo: EmprestimoDTO = await respostaAPI.json();
+                return Emprestimo;
+            } else {
+                throw new Error("Não foi possível buscar o emprestimo.");
+            }
+        } catch (error) {
+            console.error(`Erro ao fazer a consulta de emprestimo por ID. ${error}`);
             return;
         }
     }
